eccf1f74bc7853bbf5c415e20818ad924831cf1b
Alexander Burton <amacburton@gmail.com>
2017-01-13 10:26:59 -0600

    working

--- main.cpp
+++ main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  LLTemplate
-//
-//  Created by James Shockey on 12/6/16.
-//  Copyright © 2016 James Shockey. All rights reserved.
-//
-
 #include <iostream>
 #include <fstream>
 #include <cmath> 
+#include <vector>
 #include "slist.h"
 
 using namespace std;
@@ -16,80 +9,76 @@ using namespace std;
 class Airport
 {
 public:
-    char    code[5];
-    double   longitude;
-    double   latitude;
+    char	code[5];
+    double	longitude;
+    double	latitude;
     
 };
 
-
-
-void simpleSortTotal(Airport* s[], int c);
 double distanceEarth(double lat1d, double lon1d, double lat2d, double lon2d);
 
 int main()
 {
     ifstream infile;
-    int i=0;
+    int i = 0;
     char cNum[10] ;
-    Airport* airportArr[13500];
-    int   airportCount;
-    //Airport* a[13500];
+    LinkedList<Airport*> airportArr;
+    int airportCount;
+	int c = 0;
+	
+	Airport* aus;
     
     infile.open ("./USAirportCodes.csv", ifstream::in);
-    if (infile.is_open())
-    {
-        int   c=0;
-        while (infile.good())
-        {
-            airportArr[c] = new Airport();
-            infile.getline(airportArr[c]->code, 256, ',');
-            infile.getline(cNum, 256, ',');
-            airportArr[c]->longitude = atof(cNum);
-            infile.getline(cNum, 256, '\n');
-            airportArr[c]->latitude = atof(cNum);
-
-            if (!(c % 1000))
-                cout << airportArr[c]->code << " long: " << airportArr[c]->longitude << " lat: " << airportArr[c]->latitude <<  endl;
-
-            /*
-            if (!(c % 1000))
-            {
-                cout << airportArr[c]->code << " long: " << airportArr[c]->longitude << " lat: " << airportArr[c]->latitude <<  endl;
-                cout << airportArr[c+1]->code << endl; //" long: " << airportArr[c+1]->longitude << " lat: " << airportArr[c+1]->latitude <<  endl;                               
-            }
-            */
-
+    if (infile.is_open()) {
+    	// skip first line
+    	string dummyLine;
+    	getline(infile, dummyLine);
+        while (infile.good()) {
+        	Airport* airport = new Airport();
+            airportArr.push(airport);
+            infile.getline(airport->code, 256, ',');
             
-            i++ ;
-            c++;
+            infile.getline(cNum, 256, ',');
+            airport->longitude = atof(cNum);
+            infile.getline(cNum, 256, '\n');
+            airport->latitude = atof(cNum);
+            
+            if (strcmp(airport->code,"AUS") == 0) {
+            	cout << "AUS found" << endl;
+            	aus = airport;
+            }
+            ++i, ++c;
         }
         airportCount = c-1;
         infile.close();
-        
-         for (int c=0; c < airportCount; c++)
-            if (!(c % 1000))
-            {
-                cout << airportArr[c]->code << " long: " << airportArr[c]->longitude << " lat: " << airportArr[c]->latitude <<  endl;
-                cout << airportArr[c+1]->code << " long: " << airportArr[c+1]->longitude << " lat: " << airportArr[c+1]->latitude <<  endl;
-                cout <<"Distance between " << airportArr[c]->code << " and " << airportArr[c+1]->code << " is "
-                  << distanceEarth( airportArr[c]->longitude, airportArr[c]->latitude , airportArr[c+1]->longitude, airportArr[c+1]->latitude) << endl;
-            }
-
-
-
     }
     else
     {
         cout << "Error opening file";
     }
- 
-
-
-   // Initialize Linked List
-   
-   // 
-
+    
+    cout << "Num airports: " << airportCount << endl;
+    Airport* max = aus;
+    double max_d = 0;
+    vector<Airport*> within_100;
+    for (int i = 0; i < airportArr.size(); ++i) {
+    	Airport* current = airportArr[i];
+    	if (current != aus) {
+    		double dist = distanceEarth(aus->longitude, aus->latitude, current->longitude, current->latitude);
+    		if (dist > max_d) {
+    			max = current;
+    			max_d = dist;
+    		}
+    		if (dist <= 100) {
+    			within_100.push_back(current);
+    		}
+    	}
+    }
+    cout << "Max dist: " << max->code << " with distance " << max_d << endl;
+    cout << "Within 100km: ";
+    for (int i = 0; i < within_100.size(); ++i) {
+    	cout << within_100[i]->code << ",";
+    }
     
 }
 
@@ -98,26 +87,12 @@ int main()
 #define pi 3.14159265358979323846
 #define earthRadiusKm 6371.0
 
-// This function converts decimal degrees to radians
 double deg2rad(double deg) {
   return (deg * pi / 180);
 }
-
-//  This function converts radians to decimal degrees
 double rad2deg(double rad) {
   return (rad * 180 / pi);
 }
-
-
-/**
- * Returns the distance between two points on the Earth.
- * Direct translation from http://en.wikipedia.org/wiki/Haversine_formula
- * @param lat1d Latitude of the first point in degrees
- * @param lon1d Longitude of the first point in degrees
- * @param lat2d Latitude of the second point in degrees
- * @param lon2d Longitude of the second point in degrees
- * @return The distance between the two points in kilometers
- */
 double distanceEarth(double lat1d, double lon1d, double lat2d, double lon2d) {
   double lat1r, lon1r, lat2r, lon2r, u, v;
   lat1r = deg2rad(lat1d);
@@ -128,12 +103,3 @@ double distanceEarth(double lat1d, double lon1d, double lat2d, double lon2d) {
   v = sin((lon2r - lon1r)/2);
   return 2.0 * earthRadiusKm * asin(sqrt(u * u + cos(lat1r) * cos(lat2r) * v * v));
 }
-
-
-
-/*
-void simpleSortTotal()
-{
-
-}
-*/
--- slist.h
+++ slist.h
@@ -1,32 +1,157 @@
-/*
-
-Header file for single linked list class library
-
-*/
-
-
-// add(value)				//Adds a new value to the end of this list.
-
-// clear()					//Removes all elements from this list.
-
-// equals(list)				//Returns true if the two lists contain the same elements in the same order.
-
-//get(index)				//Returns the element at the specified index in this list.
-
-//insert(index, value)		//Inserts the element into this list before the specified index.
-
-//exchg(index1, index2)		//Switches the payload data of specified indexex.
-
-// isEmpty()				//Returns true if this list contains no elements.
-
-// mapAll(fn)				//Calls the specified function on each element of the linkedlist in ascending index order.
-
-// remove(index)			//Removes the element at the specified index from this list.
-
-// set(index, value)		//Replaces the element at the specified index in this list with a new value.
-
-// size()					//Returns the number of elements in this list.
-
-// subList(start, length)	//Returns a new list containing elements from a sub-range of this list.
-
-// toString()				//Converts the list to a printable string representation.
+#include <iostream>
+#include <stdexcept>
+ 
+template <typename T> class LinkedList {
+	class Node {
+	public:
+		T value;
+		void setNext(Node* n) {
+			next = n;
+		}
+		void setPrev(Node* n) {
+			prev = n;
+		}
+		Node* next;
+		Node* prev;
+	};
+public:
+	LinkedList() { // Initial empty list conditions
+		head = nullptr;
+		tail = nullptr;
+		length = 0;
+	}
+	~LinkedList() { // Removes each Node
+		while (head != nullptr && tail != nullptr) pop(); // Pop each value to clear memory
+	}
+ 
+	unsigned int size() { // Returns the size of the list
+		return length;
+	}
+ 
+	T &operator[](unsigned int index) { // Returns the value at the given index
+		Node* node = head;
+		for (int i = 0; i < index; ++i) {
+			node = node->next;
+			if (node == nullptr) {
+				std::string error("Offset of "+std::to_string(index)+" out of range");
+				throw std::range_error(std::string(error));
+			}
+		}
+		return node->value;
+	}
+ 
+	void add(T val) { // Adds a value to the beginning of the list
+		Node* newNode = new Node();
+		newNode->value = val;
+		if (head != nullptr) {
+			head->setPrev(newNode);
+		}
+		newNode->next = head;
+		newNode->prev =	nullptr;
+		head = newNode;
+		if (length == 0) {
+			tail = newNode;
+		}
+ 
+		length++;
+	}
+	void push(T val) { // Pushes a value to the end of the list
+		Node* newNode = new Node();
+		newNode->value = val;
+		if (tail != nullptr) {
+			tail->setNext(newNode);
+		}
+		newNode->prev = tail;
+		newNode->next =	nullptr;
+		tail = newNode;
+		if (length == 0) {
+			head = newNode;
+		}
+ 
+		length++;
+	}
+	void insert(int index, T val) { // Inserts a value at index in the list
+		if (index == length-1) {
+			this->push(val);
+			return;
+		}
+		Node* newNode = new Node();
+		newNode->value = val;
+		Node* before = this->nodeAt(index);
+		Node* after = before->next;
+ 
+		newNode->next = after;
+		newNode->prev = before;
+		before->next = newNode;
+		after->prev = newNode;
+		length++;
+	}
+ 
+	T pop() { // Pops a value from the beginning and returns it
+		if (head == nullptr || tail == nullptr) {
+			std::string error("Cannot pop empty list");
+			throw std::range_error(error);
+		}
+		Node* n = head;
+		T ret = n->value;
+		head = head->next;
+		if (head != nullptr) {
+			head->prev = nullptr;
+		}
+		delete n;
+ 
+		length--;
+		return ret;
+	}
+	T shift() { // Shifts a value from the end and returns it
+		if (head == nullptr || tail == nullptr) {
+			std::string error("Cannot shift empty list");
+			throw std::range_error(error);
+		}
+		Node* n = tail;
+		T ret = n->value;
+		tail = tail->prev;
+		if (tail != nullptr) {
+			tail->next = nullptr;
+		}
+		delete n;
+ 
+		length--;
+		return ret;
+	}
+	T remove(int index) {
+		if (index == 0) {
+			return this->pop();
+		}
+		if (index == length-1) {
+			return this->shift();
+		}
+		Node* n = this->nodeAt(index);
+		T ret = n->value;
+		Node* before = n->prev;
+		Node* after = n->next;
+		before->next = after;
+		after->prev = before;
+		delete n;
+ 
+		length--;
+		return ret;
+	}
+ 
+private:
+	Node* nodeAt(int index) {
+		Node* node = head;
+		for (int i = 0; i < index; ++i) {
+			node = node->next;
+			if (node == nullptr) {
+				std::string error("Offset of "+std::to_string(index)+" out of range");
+				throw 1;
+			}
+		}
+		return node;
+	}
+ 
+	Node* head;
+	Node* tail;
+	int length;
+};
